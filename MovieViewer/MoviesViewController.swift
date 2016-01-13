035c9eef7306eab79b46120c8588c669b3ddd73e
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-01-12 20:27:09 -0800

    downcast variable cell to be type MovieCell
    - so can access IBOutlet variable in custom MovieCell class
    (MovieCell.swift), titleLable and overviewLabel

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -64,7 +64,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
